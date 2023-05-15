5c7124a66d5470ae286506dccaeaccef7aa2e548
lakshmieee <shivahari583@gmail.com>
2023-05-15 11:23:19 +0530

    reactive form added for contact component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
 
+contactDetails =new FormGroup({
+name :new FormControl('',Validators.required),
+email :new FormControl('',Validators.email),
+subject :new FormControl('',Validators.required),
+comments :new FormControl('',Validators.maxLength(500)),
+})
+
+  showDetails(){
+    console.log(this.contactDetails.value);
+  }
 }
